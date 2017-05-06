862e69e3464b3ea0e65fc9e965dfaa6f0db4e632
Trenton Mcleod <Boanerges@Trentons-MBP.attlocal.net>
2017-05-06 09:22:21 -0400

    eat

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burger(burger_name, devoured, created_at)
+VALUES ("Whata Burger", false, "2016-11-05 04:20:00"),
+{
+	("Cheese Burger", false, "2016-11-05 04:20:01"),
+	("Bacon Chedder Burger", false, "2016-11-05 04:20:02");
+}
